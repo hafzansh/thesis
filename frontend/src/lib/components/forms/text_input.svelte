23b9631ed88f6e9c4d775e2c8e3e4623eb172d06
Hafiz Anshori <hafizansh.m@gmail.com>
2022-07-11 17:25:11 +0800

    sveltekit auth-token login session

---
+++ frontend/src/lib/components/forms/text_input.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+  import type { IconDefinition } from "@fortawesome/free-solid-svg-icons";
+  import Fa from "svelte-fa";
+  export let icon: IconDefinition;
+  export let id:string;  
+  export let inputName = "text";
+  export let label = "";
+  export let placeholder = "";
+  export let value = "";
+  export let error = false;
+  export let focused = false;
+  export let required:boolean;
+</script>
+
+<div class="flex flex-col mb-5">
+  <label for="text" class="mb-1 text-xs tracking-wide text-gray-600"
+    >{label}</label
+  >
+  <div class="relative">
+    <div
+      class="
+              inline-flex
+              items-center
+              justify-center
+              absolute
+              left-0
+              top-0
+              h-full
+              w-10
+              text-gray-400
+            "
+    >
+      <Fa icon={icon} class="h-4 w-4 text-blue-500" />
+    </div>
+
+    <!-- svelte-ignore a11y-autofocus -->
+    <input
+      id={id}
+      bind:value
+      autofocus={focused}
+      name={inputName}
+      {required}
+      on:input={() => (error = false)}
+      class="
+              text-sm
+              placeholder-gray-500
+              pl-10
+              pr-4
+              rounded-2xl
+              border border-gray-400
+              w-full
+              py-2
+              focus:outline-none focus:border-blue-400
+            "
+      class:invalidInput={error}
+      {placeholder}
+    />
+  </div>
+</div>
--- frontend/src/routes/login/index.svelte
+++ frontend/src/routes/login/index.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
   import { send } from "../../lib/shared/api";
-  import Alert from "@comp/notification/alert.svelte"
+  import Fa from "svelte-fa";
+  import { faAt,faLock } from "@fortawesome/free-solid-svg-icons";
+  import TextInput from "@lib/components/forms/text_input.svelte"
+  import Alert from "@comp/notification/alert.svelte";
   export let error: string;
   async function login(event: SubmitEvent) {
     const formEl = event.target as HTMLFormElement;
@@ -11,40 +14,116 @@
   }
 </script>
 
-<div class="hero min-h-screen bg-base-200">
-  <div class="hero-content flex-col lg:flex-row-reverse">
-    <div class="text-center lg:text-left">
-      <h1 class="text-5xl font-bold">Login now!</h1>
-      <p class="py-6">
-        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-        excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a
-        id nisi.
-      </p>
-    </div>
-    <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-      <div class="card-body">
-        <form on:submit|preventDefault={login} method="post">
-          <div>
-            <div class="label">
-              <label class="label-text" for="username">Username</label>
-            </div>
-            <input class="input input-bordered w-full max-w-xs" id="username" name="username" type="text" required />
+<div class="flex flex-col min-h-screen bg-gray-200 justify-center items-center">
+  <div
+    class="flex flex-col
+    bg-white
+    shadow-md
+    px-4
+    sm:px-6
+    md:px-8
+    lg:px-10
+    py-8
+    rounded-3xl
+    w-50
+    max-w-md"
+  >
+  <div class="font-medium self-center text-xl sm:text-3xl text-gray-800">
+    Welcome Back
+  </div>
+  <div class="mt-4 self-center text-xl sm:text-sm text-gray-800">
+    Enter your credentials to access your account
+  </div>
+  <div class="mt-10">
+    <form on:submit|preventDefault={login} method="post">
+      <div>
+        <div class="label">
+          <label class="label-text" for="username">Username</label>
+        </div>
+        <TextInput
+            icon={faAt}             
+            error={false}
+            focused={true}
+            id="username"
+            inputName="username"
+            placeholder="username"
+            required={true}
+        />
+
+        <div>
+          <div class="label">
+            <label class="label-text" for="username">Username</label>
           </div>
-
-          <div>
-            <div class="label">
-                <label class="label-text" for="username">Username</label>
+          <div class="relative">
+            <div
+              class="
+                inline-flex
+                items-center
+                justify-center
+                absolute
+                left-0
+                top-0
+                h-full
+                w-10
+                text-gray-400
+              "
+            >
+              <Fa icon={faLock} />
             </div>
-            <input class="input input-bordered w-full max-w-xs" id="password" name="password" type="password" required />
+          <input
+            class="input input-bordered pl-10 w-full max-w-xs rounded-2xl focus:outline-none focus:border-blue-400"
+            id="password"
+            name="password"
+            type="password"
+            required
+          />
+        </div>
+
+        {#if error}
+          <Alert message={error} />
+        {/if}
+
+        <div class="flex w-full">
+            <button
+              type="submit"
+              class="
+                flex
+                mt-2
+                items-center
+                justify-center
+                focus:outline-none
+                text-white text-sm
+                sm:text-base
+                bg-blue-500
+                hover:bg-blue-600
+                rounded-2xl
+                py-2
+                w-full
+                transition
+                duration-150
+                ease-in
+              "
+            >
+              <span class="mr-2 uppercase">Sign In</span>
+              <span>
+                <svg
+                  class="h-6 w-6"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+              </span>
+            </button>
           </div>
-
-          {#if error}
-            <Alert message={error}/>
-          {/if}
-
-          <button class="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg" type="submit">Sign In</button>
-        </form>
       </div>
-    </div>
+    </form>
+</div>
   </div>
 </div>
